95e5b153d66dfac2d7438bd803c2de5592cf6c71
Dylan Peters <dlp22@duke.edu>
2019-01-22 19:41:05 -0500

    Saved admin user.

--- src/database_setup.js
+++ src/database_setup.js
@@ -1,13 +1,20 @@
 const database_library = require('./database.js');
+const crypto = require('crypto');
 
-var successCallback = function() {
-    console.log("it was a success");
-};
-
-var failCallback = function() {
-    console.log("fail");
-};
-
-var db = new database_library.Database(successCallback, failCallback);
-// var UserClass = new database_library.UserClass();
-// console.log(admin);
+let db = database_library.Database;
+let admin_salt = crypto.randomBytes(16).toString('hex');
+let admin = new database_library.userModel({
+    username: 'admin',
+    salt: admin_salt,
+    saltedHashedPassword: crypto.pbkdf2Sync('password', admin_salt, 1000, 64, 'sha512').toString('hex'),
+    token: crypto.randomBytes(16).toString('hex')
+});
+admin.save().then(
+    doc => {
+        console.log(doc);
+    }
+).catch(
+    err => {
+        console.log(err);
+    }
+);
