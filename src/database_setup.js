f9b26582e618febd41e07ec983c66bb033d7d6ce
Dylan Peters <dlp22@duke.edu>
2019-01-24 17:29:41 -0500

    Changed database setup.

--- src/database_setup.js
+++ src/database_setup.js
@@ -19,19 +19,19 @@ admin.save().then(
     }
 );
 
-// let user1_salt = crypto.randomBytes(16).toString('hex');
-// let user1 = new database_library.userModel({
-//     username: 'user1',
-//     salt: user1_salt,
-//     saltedHashedPassword: crypto.pbkdf2Sync('password', user1_salt, 1000, 64, 'sha512').toString('hex'),
-//     token: crypto.randomBytes(16).toString('hex')
-// });
-// user1.save().then(
-//     doc => {
-//         console.log(doc);
-//     }
-// ).catch(
-//     err => {
-//         console.log(err);
-//     }
-// );
+let user1_salt = crypto.randomBytes(16).toString('hex');
+let user1 = new database_library.userModel({
+    username: 'user1',
+    salt: user1_salt,
+    saltedHashedPassword: crypto.pbkdf2Sync('password', user1_salt, 1000, 64, 'sha512').toString('hex'),
+    token: crypto.randomBytes(16).toString('hex')
+});
+user1.save().then(
+    doc => {
+        console.log(doc);
+    }
+).catch(
+    err => {
+        console.log(err);
+    }
+);
