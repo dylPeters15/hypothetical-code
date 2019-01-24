20335998e3a1af8f3eb01467e8561955a862e9d4
Dylan Peters <dlp22@duke.edu>
2019-01-23 22:03:08 -0500

    Added route guards.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,35 +1,32 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NavigationComponent } from './navigation/navigation.component';
-import {RouterModule, Routes} from "@angular/router";
-import {CustomMaterialModule} from "./core/material.module";
-import { FooterComponent } from './footer/footer.component';
-import { LoginComponent } from './login/login.component';
-import { UserComponent } from './user/user.component';
-
-import {FormsModule} from '@angular/forms';
-import {AppRoutingModule} from './app-routing.module'
-
 import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule, Routes } from "@angular/router";
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CustomMaterialModule } from "./core/material.module";
+import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
-
+import { LoginComponent } from './login/login.component';
+import { NavigationComponent } from './navigation/navigation.component';
 import { UserRouteGuardService } from './user-route-guard.service';
 import { AdminRouteGuardService } from './admin-route-guard.service';
+import { UserComponent } from './user/user.component';
 
 const appRoutes: Routes = [
-  { path: '', component: LoginComponent, data: { title: 'Log In' } },
   { path: 'login', component: LoginComponent, data: { title: 'Log In' } },
   { path: 'home', component: HomeComponent, data: { title: 'Home' }, canActivate: [UserRouteGuardService] },
-  { path: 'manufacturing-calculator', component: HomeComponent, data: { title: 'Manufacturing Calculator' } },
-  { path: 'ingredient-dependency-report', component: HomeComponent, data: { title: 'Ingredient Dependency Report' } },
-  { path: 'manufacturing-goal', component: HomeComponent, data: { title: 'Manufacturing Goals' } },
-  { path: 'ingredient-inventory', component: HomeComponent, data: { title: 'Ingredient Inventory' } },
-  { path: 'sku-inventory', component: HomeComponent, data: { title: 'SKU Inventory' } },
-  { path: 'product-line-inventory', component: HomeComponent, data: { title: 'Product Line Inventory' } },
-  { path: 'import-export', component: HomeComponent, data: { title: 'Import Export' } },
+  { path: 'user-management', component: HomeComponent, data: { title: 'User Management' }, canActivate: [AdminRouteGuardService] },
+  { path: 'account-settings', component: HomeComponent, data: { title: 'Account Settings' }, canActivate: [UserRouteGuardService] },
+  { path: 'manufacturing-calculator', component: HomeComponent, data: { title: 'Manufacturing Calculator' }, canActivate: [UserRouteGuardService] },
+  { path: 'ingredient-dependency-report', component: HomeComponent, data: { title: 'Ingredient Dependency Report' }, canActivate: [UserRouteGuardService] },
+  { path: 'manufacturing-goal', component: HomeComponent, data: { title: 'Manufacturing Goals' }, canActivate: [UserRouteGuardService] },
+  { path: 'ingredient-inventory', component: HomeComponent, data: { title: 'Ingredient Inventory' }, canActivate: [AdminRouteGuardService] },
+  { path: 'sku-inventory', component: HomeComponent, data: { title: 'SKU Inventory' }, canActivate: [AdminRouteGuardService] },
+  { path: 'product-line-inventory', component: HomeComponent, data: { title: 'Product Line Inventory' }, canActivate: [AdminRouteGuardService] },
+  { path: 'import-export', component: HomeComponent, data: { title: 'Import Export' }, canActivate: [AdminRouteGuardService] },
   { path: '**', redirectTo: 'home' }
 ];
 
