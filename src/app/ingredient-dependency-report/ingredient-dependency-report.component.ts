6cc5e0d427c9c90d9e7a14fb3ea90511ea7a0c3b
Faith <faithrodriguez08@gmail.com>
2019-02-04 12:52:05 -0800

    Connected DB

--- src/app/ingredient-dependency-report/ingredient-dependency-report.component.ts
+++ src/app/ingredient-dependency-report/ingredient-dependency-report.component.ts
@@ -1,39 +1,60 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
-import {MatSort, MatTableDataSource} from '@angular/material';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { RestService } from '../rest.service';
+import {MatSnackBar} from '@angular/material';
+import { MatDialogRef, MatDialog, MatSort, MatTableDataSource, MatPaginator } from "@angular/material";
+import { MoreInfoDialogComponent } from '../more-info-dialog/more-info-dialog.component';
+import { NewIngredientDialogComponent } from '../new-ingredient-dialog/new-ingredient-dialog.component';
+import { AfterViewChecked } from '@angular/core';
 
 export interface IngredientData {
   // completion: boolean;
-  ingredient: string;
+  ingredientName: string;
+  ingredientNumber: number;
   numberSKUs: number;
   SKUs: string;
 }
 
-const INGREDIENT_DATA: IngredientData[] = [
-  // {completion: false, ingredient: 'Chicken', numberSKUs: 2, SKUs: 'Chicken Noodle Soup; Chicken Tenders'},
-  // {completion: false, ingredient: 'Salt', numberSKUs: 2, SKUs: 'Chicken Noodle Soup; Tortillas'},
-  // {completion: false, ingredient: 'Flour', numberSKUs: 1, SKUs: 'Tortillas'}
-  {ingredient: 'Chicken', numberSKUs: 2, SKUs: 'Chicken Noodle Soup; Chicken Tenders'},
-  {ingredient: 'Salt', numberSKUs: 2, SKUs: 'Chicken Noodle Soup; Tortillas'},
-  {ingredient: 'Flour', numberSKUs: 1, SKUs: 'Tortillas'}
-];
+// const INGREDIENT_DATA: IngredientData[] = [
+//   // {completion: false, ingredient: 'Chicken', numberSKUs: 2, SKUs: 'Chicken Noodle Soup; Chicken Tenders'},
+//   // {completion: false, ingredient: 'Salt', numberSKUs: 2, SKUs: 'Chicken Noodle Soup; Tortillas'},
+//   // {completion: false, ingredient: 'Flour', numberSKUs: 1, SKUs: 'Tortillas'}
+//   {ingredient: 'Chicken', numberSKUs: 2, SKUs: 'Chicken Noodle Soup; Chicken Tenders'},
+//   {ingredient: 'Salt', numberSKUs: 2, SKUs: 'Chicken Noodle Soup; Tortillas'},
+//   {ingredient: 'Flour', numberSKUs: 1, SKUs: 'Tortillas'}
+// ];
  
-/**
- * @title Basic use of `<table mat-table>`
- */
 @Component({
   selector: 'app-ingredient-dependency-report',
   styleUrls: ['./ingredient-dependency-report.component.css'],
   templateUrl: './ingredient-dependency-report.component.html',
 })
 export class IngredientDependencyComponent implements OnInit {
-  // displayedColumns: string[] = ['completion','ingredient', 'numberSKUs', 'SKUs'];
-  displayedColumns: string[] = ['ingredient', 'numberSKUs', 'SKUs'];
-  dataSource = new MatTableDataSource(INGREDIENT_DATA);
+  displayedColumns: string[] = ['ingredientName', 'ingredientNumber', 'numberSKUs', 'SKUs'];
+  constructor(public rest:RestService, private snackBar: MatSnackBar, private dialog: MatDialog) { }
+  data: IngredientData[] = [];
+  dialogRef: MatDialogRef<MoreInfoDialogComponent>;
+  newDialogRef: MatDialogRef<NewIngredientDialogComponent>;
+  dataSource =  new MatTableDataSource<IngredientData>(this.data);
 
   @ViewChild(MatSort) sort: MatSort;
 
   ngOnInit() {
-    this.dataSource.sort = this.sort;
+    this.refreshData();
+  }
+
+  refreshData() {
+    this.rest.getIngredients().subscribe(response => {
+      this.data = response;
+      // this.data.forEach(user => {
+      //   user['checked'] = false;
+      // });
+      console.log(this.data);
+      // this.sortData();
+      this.dataSource.sort = this.sort;
+      this.dataSource =  new MatTableDataSource<IngredientData>(this.data);
+    // this.dataSource.paginator = this.paginator;
+    });
+    
   }
 
   applyFilter(filterValue: string) {
