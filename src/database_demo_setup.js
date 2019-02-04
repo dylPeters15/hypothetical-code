4f7953d0325ee10e455d5638d0df92df636b84fe
Dylan Peters <dlp22@duke.edu>
2019-02-04 14:41:52 -0500

    Created clear database file.

--- src/database_demo_setup.js
+++ src/database_demo_setup.js
@@ -2,22 +2,6 @@ const database_library = require('./database.js');
 const crypto = require('crypto');
 
 let db = database_library.Database;
-let admin_salt = crypto.randomBytes(16).toString('hex');
-let admin = new database_library.userModel({
-    username: 'admin',
-    salt: admin_salt,
-    saltedHashedPassword: crypto.pbkdf2Sync('password', admin_salt, 1000, 64, 'sha512').toString('hex'),
-    token: crypto.randomBytes(16).toString('hex')
-});
-admin.save().then(
-    doc => {
-        console.log(doc);
-    }
-).catch(
-    err => {
-        console.log(err);
-    }
-);
 for (i = 0; i < 10; i++) {
     let user1_salt = crypto.randomBytes(16).toString('hex');
     let user1 = new database_library.userModel({
@@ -52,20 +36,3 @@ testGoal.save().then(
         console.log(err);
     }
 );
-
-// let user1_salt = crypto.randomBytes(16).toString('hex');
-// let user1 = new database_library.userModel({
-//     username: 'user1',
-//     salt: user1_salt,
-//     saltedHashedPassword: crypto.pbkdf2Sync('password', user1_salt, 1000, 64, 'sha512').toString('hex'),
-//     token: crypto.randomBytes(16).toString('hex')
-// });
-// user1.save().then(
-//     doc => {
-//         console.log(doc);
-//     }
-// ).catch(
-//     err => {
-//         console.log(err);
-//     }
-// );
