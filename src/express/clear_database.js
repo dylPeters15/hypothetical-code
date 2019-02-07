8a239df591e93bb30de9abb1cd1766c2c8afb732
Dylan Peters <dlp22@duke.edu>
2019-02-07 17:34:36 -0500

    Changed syntax.

--- src/express/clear_database.js
+++ src/express/clear_database.js
@@ -6,8 +6,8 @@ mongoose.connect('mongodb://localhost/my-test-db',function(){
     mongoose.connection.db.dropDatabase();
     console.log("Database cleared.");
     user_utils.getUsers();
-    user_utils.createUser("admin","hypocode123").then(function(value) {
+    user_utils.createUser("admin","hypocode123").then(value => {
         console.log("Value: ",value);
-    });
+    });//can also add here .catch(err => {}) but choosing not to do so because we don't want to catch this error - the program should fail if there is an error here
     console.log("Admin user created.");
 });
