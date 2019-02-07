f921627495b2626a8af996602b0f02a74a99dcff
Dylan Peters <dlp22@duke.edu>
2019-02-07 17:37:35 -0500

    Improved syntax in clear_database.js.

--- src/express/clear_database.js
+++ src/express/clear_database.js
@@ -2,12 +2,11 @@ const mongoose = require('mongoose');
 const database = require('./database.js');
 const user_utils = require('./user_utils.js');
 
-mongoose.connect('mongodb://localhost/my-test-db',function(){
+mongoose.connect('mongodb://localhost/my-test-db').then(() => {
     mongoose.connection.db.dropDatabase();
     console.log("Database cleared.");
-    user_utils.getUsers();
     user_utils.createUser("admin","hypocode123").then(value => {
-        console.log("Value: ",value);
+        console.log("Admin user created.");
+        mongoose.connection.close();
     });//can also add here .catch(err => {}) but choosing not to do so because we don't want to catch this error - the program should fail if there is an error here
-    console.log("Admin user created.");
-});
+});//can also add here .catch(err => {}) but choosing not to do so because we don't want to catch this error - the program should fail if there is an error here
