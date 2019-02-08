ed68950c8d378b44a63bf44079c7737565925611
Dylan Peters <dlp22@duke.edu>
2019-02-08 11:17:29 -0500

    Filled in all user_utils methods.

--- src/express/user_utils.js
+++ src/express/user_utils.js
@@ -12,44 +12,94 @@ function generateSaltAndHash(password) {
 
 //need to ensure that token is unique
 function generateToken() {
-    return crypto.randomBytes(16).toString('hex');
+    return new Promise((resolve, reject) => {
+        database.userModel.find({}).select('token -_id').exec((err, tokens) => {
+            var token = crypto.randomBytes(16).toString('hex');
+            var maxIterations = 10000;
+            var iterations = 0;
+            while (tokens.includes(token)) {
+                token = crypto.randomBytes(16).toString('hex');
+                iterations = iterations + 1;
+                if (iterations >= maxIterations) {
+                    reject("Could not generate token.");
+                }
+            }
+            resolve(token);
+        });
+    });
 }
 
 function getUsers(userName, userNameRegex, limit) {
-    return new Promise(function (resolve, reject) {
-        database.userModel.findOne({userName:userName}, (err, user) => {
+    userName = userName || "";
+    userNameRegex = userNameRegex || "$a";
+    limit = limit || database.defaultSearchLimit;
+
+    return new Promise((resolve, reject) => {
+        var filterSchema = {
+            $or: [
+                { userName: userName },
+                { userName: { $regex: userNameRegex } }
+            ]
+        }
+        database.userModel.find(filterSchema).limit(limit).exec((err, users) => {
             if (err) {
                 reject(Error(err));
             } else {
-                resolve(user);
+                resolve(users);
             }
         });
     });
 }
 
 function createUser(username, password) {
-    return new Promise(function (resolve, reject) {
+    return new Promise((resolve, reject) => {
         let saltAndHash = generateSaltAndHash(password);
-        let user = new database.userModel({
-            userName: username,
-            salt: saltAndHash.salt,
-            saltedHashedPassword: saltAndHash.hash,
-            token: generateToken()
-        });
-        user.save().then(response => {
-            resolve(response);
+        generateToken().then(token => {
+            let user = new database.userModel({
+                userName: username,
+                salt: saltAndHash.salt,
+                saltedHashedPassword: saltAndHash.hash,
+                token: token
+            });
+            user.save().then(response => {
+                resolve(response);
+            }).catch(err => {
+                reject(Error(err));
+            });
         }).catch(err => {
             reject(Error(err));
         });
     });
 }
 
-function modifyUser(searchCriteria, username, password) {
-
+function modifyUser(userName, newUserObject) {
+    return new Promise((resolve, reject) => {
+        var filterSchema = {
+            userName: userName
+        }
+        database.userModel.updateOne(filterSchema, newUserObject, (err, response) => {
+            if (err) {
+                reject(Error(err));
+            } else {
+                resolve(response);
+            }
+        });
+    });
 }
 
 function deleteUser(searchCriteria) {
-
+    return new Promise((resolve, reject) => {
+        var filterSchema = {
+            userName: userName
+        }
+        database.userModel.deleteOne(filterSchema, (err, response) => {
+            if (err) {
+                reject(Error(err));
+            } else {
+                resolve(response);
+            }
+        });
+    });
 }
 
 module.exports = {
--- src/express/user_utils_spec.js
+++ src/express/user_utils_spec.js
@@ -2,9 +2,23 @@ const mongoose = require('mongoose');
 const database = require('./database.js');
 const user_utils = require('./user_utils.js');
 
-user_utils.getUsers("admin").then(user => {
-    console.log(user);
-    mongoose.connection.close();
+// user_utils.getUsers("admin").then(users => {
+//     console.log("Should only log admin: ", users);
+// }).catch(err => {
+//     console.log(Error(err));
+// });
+
+user_utils.getUsers(null, "a.*").then(users => {
+    console.log("Should log all users starting with a: ", users);
 }).catch(err => {
     console.log(Error(err));
 });
+
+// user_utils.createUser("asdf","pass").then(response => {
+//     console.log(response);
+// }).catch(err => {
+//     console.log(Error(err));
+// });
+
+//    mongoose.connection.close();
+
