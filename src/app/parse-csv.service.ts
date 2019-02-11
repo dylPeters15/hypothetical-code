5f3245bc5438426403ed68b6d919582ce10235b2
Dylan Peters <dlp22@duke.edu>
2019-02-11 11:25:38 -0500

    Almost finished.

--- src/app/parse-csv.service.ts
+++ src/app/parse-csv.service.ts
@@ -27,7 +27,30 @@ export class ParseCsvService {
           filesWithText[file.name] = result;
           numFilesRead = numFilesRead + 1;
           if (numFilesRead == filesAsArray.length) {
-            resolve(this.parseFilesWithNames(filesWithText));
+            this.parseFilesWithNames(filesWithText).then(result => {
+              var objectToReturn = {};
+              objectToReturn['skus'] = [];
+              objectToReturn['ingredients'] = [];
+              objectToReturn['formulas'] = [];
+              objectToReturn['productlines'] = [];
+              for (let filename in result) {
+                if (filename.startsWith("skus")) {
+                  objectToReturn['skus'] = objectToReturn['skus'].concat(result[filename])
+                } else if (filename.startsWith("ingredients")) {
+                  objectToReturn['ingredients'] = objectToReturn['ingredients'].concat(result[filename])
+                } else if (filename.startsWith("formulas")) {
+                  objectToReturn['formulas'] = objectToReturn['formulas'].concat(result[filename])
+                } else if (filename.startsWith("product_lines")) {
+                  objectToReturn['productlines'] = objectToReturn['productlines'].concat(result[filename])
+                } else {
+                  reject(Error("Filename incorrect."));
+                }
+              }
+              console.log(objectToReturn);
+              resolve(objectToReturn);
+            }).catch(err => {
+              reject(err);
+            });
           }
         }
         fileReader.readAsText(file);
@@ -35,39 +58,101 @@ export class ParseCsvService {
     });
   }
 
-   /**
-   * Return value:
-   * {
-   *    <filename1> : [
-   *        {object from line 1},
-   *        {object from line 2},
-   *        ...
-   *    ],
-   *    <filename1> : [
-   *        {object from line 1},
-   *        {object from line 2},
-   *        ...
-   *    ],
-   *    ...
-   * }
-   * @param filesAsArray 
-   */
-  parseFilesWithNames(filesWithNames: { [fileName: string]: string }): any {
-    console.log(filesWithNames);
-    for (let fileName in filesWithNames) {
-      console.log("Filename: ", fileName);
-      console.log(filesWithNames[fileName]);
-      // _.parse(filesWithNames[fileName],{});
-      // console.log(csvparse(filesWithNames[fileName]));
-      this.papa.parse(filesWithNames[fileName],{
-        complete: (result) => {
-            console.log('Parsed: ', result);
+  /**
+  * Return value:
+  * {
+  *    <filename1> : [
+  *        {object from line 1},
+  *        {object from line 2},
+  *        ...
+  *    ],
+  *    <filename1> : [
+  *        {object from line 1},
+  *        {object from line 2},
+  *        ...
+  *    ],
+  *    ...
+  * }
+  * @param filesAsArray 
+  */
+  parseFilesWithNames(filesWithNames: { [fileName: string]: string }): Promise<any> {
+    return new Promise((resolve, reject) => {
+      //check filenames
+      var numFiles = 0;
+      for (let fileName in filesWithNames) {
+        var validFileBeginning = fileName.startsWith("skus")
+          || fileName.startsWith("ingredients")
+          || fileName.startsWith("product_lines")
+          || fileName.startsWith("formulas");
+        var validFileEnd = fileName.endsWith(".csv");
+        if (!validFileBeginning) {
+          reject(Error("Error. File name must start with 'skus', 'ingredients', 'product_lines', or 'formulas'."));
         }
+        if (!validFileEnd) {
+          reject(Error("Error. File name must end with '.csv'."));
+        }
+        numFiles = numFiles + 1;
+      }
+
+      var objectToReturn = {};
+      var numParsed = 0;
+      for (let fileName in filesWithNames) {
+
+        this.papa.parse(filesWithNames[fileName],
+          {
+            header: true,
+            skipEmptyLines: true,
+            dynamicTyping: true,
+            complete: (result) => {
+              console.log(result);
+              if (result.errors.length != 0) {
+                reject(result.errors[0]);
+              }
+              let objectArray = result.data;
+              // if (fileName.endsWith(".csv")) {
+              //   if (fileName.startsWith("skus")) {
+              //     objectArray = this.parseSKUs(result);
+              //   } else if (fileName.startsWith("ingredients")) {
+              //     objectArray = this.parseIngredients(result);
+              //   } else if (fileName.startsWith("product_lines")) {
+              //     objectArray = this.parseProductLines(result);
+              //   } else if (fileName.startsWith("formulas")) {
+              //     objectArray = this.parseFormulas(result);
+              //   } else {
+              //     throw Error("Error. File name must start with 'skus', 'ingredients', 'product_lines', or 'formulas'.");
+              //   }
+              // } else {
+              //   throw Error("Error. File name must end with '.csv'.");
+              // }
+              objectToReturn[fileName] = objectArray;
+              numParsed = numParsed + 1;
+              if (numParsed == numFiles) {
+                console.log(objectToReturn);
+                resolve(objectToReturn);
+              }
+            }
+          });
+      }
     });
-    }
   }
 
- 
+  // private parseSKUs(result): any[] {
+
+  // }
+
+  // private parseIngredients(result): any[] {
+
+  // }
+
+  // private parseFormulas(result): any[] {
+
+  // }
+
+  // private parseProductLines(result): any[] {
+
+  // }
+
+
   // private parseFiles(filesAsArray: File[]) {
   //   var parsedFiles = {};
   //   for (var i = 0; i < filesAsArray.length; i = i + 1) {
