d048244aa0343ce1b27304c3d167a1e30be95da7
Dylan Peters <dlp22@duke.edu>
2019-02-14 12:53:59 -0500

    Programmatically assigns all-new or all-old radio buttons.

--- src/app/import/import-preview-dialog/import-preview-dialog.component.ts
+++ src/app/import/import-preview-dialog/import-preview-dialog.component.ts
@@ -22,4 +22,42 @@ export class ImportPreviewDialogComponent implements OnInit {
     this.dialogRef.close(this.data);
   }
 
+  areAllOldSelected() {
+    return this.areAllOfSectionOld('ingredients')
+    && this.areAllOfSectionOld('formulas')
+    && this.areAllOfSectionOld('skus')
+    && this.areAllOfSectionOld('productlines')
+    && this.areAllOfSectionOld('manufacturinglines');
+  }
+
+  areAllNewSelected() {
+    return this.areAllOfSectionNew('ingredients')
+    && this.areAllOfSectionNew('formulas')
+    && this.areAllOfSectionNew('skus')
+    && this.areAllOfSectionNew('productlines')
+    && this.areAllOfSectionNew('manufacturinglines');
+  }
+
+  areAllOfSectionNew(section) {
+    for (var i = 0; i < this.data[section]['conflicts'].length; i++) {
+      console.log(this.data[section]['conflicts'][i]['select']);
+      if (this.data[section]['conflicts'][i]['select'] != 'new') {
+        console.log(false);
+        return false;
+      }
+    }
+    return true;
+  }
+
+  areAllOfSectionOld(section) {
+    for (var i = 0; i < this.data[section]['conflicts'].length; i++) {
+      console.log(this.data[section]['conflicts'][i]['select']);
+      if (this.data[section]['conflicts'][i]['select'] != 'old') {
+        console.log(false);
+        return false;
+      }
+    }
+    return true;
+  }
+
 }
