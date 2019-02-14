29d733ebc6aabbfaff50e6e6d5a4abf35e7c0947
Dylan Peters <dlp22@duke.edu>
2019-02-13 23:55:59 -0500

    Created loop outlines in each method.

--- src/app/import-match-conflict-new-checker.service.ts
+++ src/app/import-match-conflict-new-checker.service.ts
@@ -1,50 +1,204 @@
 import { Injectable } from '@angular/core';
+import { RestService } from './rest.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ImportMatchConflictNewCheckerService {
 
-  constructor() { }
+  constructor(private rest: RestService) { }
 
-  checkAll(input): any {
-    var toReturn = {};
-    toReturn['skus'] = this.checkSKUsMatchesConflictsNew(input['skus']);
-    toReturn['ingredients'] = this.checkIngredientsMatchesConflictsNew(input['ingredients']);
-    toReturn['productlines'] = this.checkProductLinesMatchesConflictsNew(input['productlines']);
-    toReturn['formulas'] = this.checkFormulasMatchesConflictsNew(input['formulas'], input['formulaingredients']);
-    toReturn['manufacturinglines'] = this.checkManufacturingLinesMatchesConflictsNew(input['manufacturinglines'], input['skumanufacturinglines']);
-    toReturn['formulaRefErrs'] = this.checkFormulaReferences(input['formulas'], input['ingredients']);
-    toReturn['skuRefErrs'] = this.checkSKUReferences(input['skus'], input['productlines'], input['formulas']);
-    toReturn['manufacturingLineRefErrs'] = this.checkManufacturingLineReferences(input['manufacturinglines'], input['skus']);
+  checkAll(input): Promise<any> {
+    return new Promise((resolve, reject) => {
+      var toReturn = {};
+      var numFinished = 0;
+      var totalNum = 8;
+      this.checkSKUsMatchesConflictsNew(input['skus']).then(result => {
+        toReturn['skus'] = result;
+        numFinished = numFinished + 1;
+        if (numFinished == totalNum) {
+          resolve(toReturn);
+        }
+      }).catch(err => {
+        reject(err);
+      });
+      this.checkIngredientsMatchesConflictsNew(input['ingredients']).then(result => {
+        toReturn['ingredients'] = result;
+        numFinished = numFinished + 1;
+        if (numFinished == totalNum) {
+          resolve(toReturn);
+        }
+      }).catch(err => {
+        reject(err);
+      });
+      this.checkProductLinesMatchesConflictsNew(input['productlines']).then(result => {
+        toReturn['productlines'] = result;
+        numFinished = numFinished + 1;
+        if (numFinished == totalNum) {
+          resolve(toReturn);
+        }
+      }).catch(err => {
+        reject(err);
+      });
+      this.checkFormulasMatchesConflictsNew(input['formulas'], input['formulaingredients']).then(result => {
+        toReturn['formulas'] = result;
+        numFinished = numFinished + 1;
+        if (numFinished == totalNum) {
+          resolve(toReturn);
+        }
+      }).catch(err => {
+        reject(err);
+      });
+      this.checkManufacturingLinesMatchesConflictsNew(input['manufacturinglines'], input['skumanufacturinglines']).then(result => {
+        toReturn['manufacturinglines'] = result;
+        numFinished = numFinished + 1;
+        if (numFinished == totalNum) {
+          resolve(toReturn);
+        }
+      }).catch(err => {
+        reject(err);
+      });
+      this.checkFormulaReferences(input['formulas'], input['ingredients']).then(result => {
+        toReturn['formulaRefErrs'] = result;
+        numFinished = numFinished + 1;
+        if (numFinished == totalNum) {
+          resolve(toReturn);
+        }
+      }).catch(err => {
+        reject(err);
+      });
+      this.checkSKUReferences(input['skus'], input['productlines'], input['formulas']).then(result => {
+        toReturn['skuRefErrs'] = result;
+        numFinished = numFinished + 1;
+        if (numFinished == totalNum) {
+          resolve(toReturn);
+        }
+      }).catch(err => {
+        reject(err);
+      });
+      this.checkManufacturingLineReferences(input['manufacturinglines'], input['skus']).then(result => {
+        toReturn['manufacturingLineRefErrs'] = result;
+        numFinished = numFinished + 1;
+        if (numFinished == totalNum) {
+          resolve(toReturn);
+        }
+      }).catch(err => {
+        reject(err);
+      });
+    });
   }
 
-  private checkSKUsMatchesConflictsNew(skus): any {
-
+  private checkSKUsMatchesConflictsNew(skus): Promise<any> {
+    return new Promise((resolve, reject) => {
+      var toReturn = {};
+      toReturn['matches'] = [];
+      toReturn['conflicts'] = [];
+      toReturn['new'] = [];
+      //do processing here
+      //can't do this yet because I'm blocked since SKU support is not ready in rest.service.ts yet
+      var numSKUsProcessed = 0;
+      skus.forEach(sku => {
+        console.log(sku);
+        numSKUsProcessed = numSKUsProcessed + 1;
+        if (numSKUsProcessed == skus.length) {
+          resolve(toReturn);
+        }
+      });
+    });
   }
 
-  private checkIngredientsMatchesConflictsNew(ingredients): any {
-
+  private checkIngredientsMatchesConflictsNew(ingredients): Promise<any> {
+    return new Promise((resolve, reject) => {
+      var toReturn = {};
+      toReturn['matches'] = [];
+      toReturn['conflicts'] = [];
+      toReturn['new'] = [];
+      var numIngredientsProcessed = 0;
+      ingredients.forEach(ingredient => {
+        console.log(ingredient);
+        //do processing here
+        numIngredientsProcessed = numIngredientsProcessed + 1;
+        if (numIngredientsProcessed == ingredients.length) {
+          resolve(toReturn);
+        }
+      });
+    });
   }
 
-  private checkProductLinesMatchesConflictsNew(productLines): any {
-
+  private checkProductLinesMatchesConflictsNew(productLines): Promise<any> {
+    return new Promise((resolve, reject) => {
+      var toReturn = {};
+      toReturn['matches'] = [];
+      toReturn['conflicts'] = [];
+      toReturn['new'] = [];
+      var numPLsProcessed = 0;
+      productLines.forEach(productLine => {
+        console.log(productLine);
+        //do processing here
+        numPLsProcessed = numPLsProcessed + 1;
+        if (numPLsProcessed == productLines.length) {
+          resolve(toReturn);
+        }
+      });
+    });
   }
 
-  private checkFormulasMatchesConflictsNew(formulas, formulaIngredients): any {
-
+  private checkFormulasMatchesConflictsNew(formulas, formulaIngredients): Promise<any> {
+    return new Promise((resolve, reject) => {
+      var toReturn = {};
+      toReturn['matches'] = [];
+      toReturn['conflicts'] = [];
+      toReturn['new'] = [];
+      var numFormulasProcessed = 0;
+      formulas.forEach(formula => {
+        console.log(formula);
+        //do processing here
+        numFormulasProcessed = numFormulasProcessed + 1;
+        if (numFormulasProcessed == formulas.length) {
+          resolve(toReturn);
+        }
+      });
+    });
   }
 
-  private checkManufacturingLinesMatchesConflictsNew(manufacturingLines, skuManufacturingLines): any {
-
+  private checkManufacturingLinesMatchesConflictsNew(manufacturingLines, skuManufacturingLines): Promise<any> {
+    return new Promise((resolve, reject) => {
+      var toReturn = {};
+      toReturn['matches'] = [];
+      toReturn['conflicts'] = [];
+      toReturn['new'] = [];
+      var numMLsProcessed = 0;
+      manufacturingLines.forEach(manufacturingLine => {
+        console.log(manufacturingLine);
+        //do processing here
+        numMLsProcessed = numMLsProcessed + 1;
+        if (numMLsProcessed == manufacturingLines.length) {
+          resolve(toReturn);
+        }
+      });
+    });
   }
 
   /**
    * Objects Formulas reference that could cause errors:
    * ingredients
    */
-  private checkFormulaReferences(formulas, ingredients): any {
-
+  private checkFormulaReferences(formulas, ingredients): Promise<any> {
+    return new Promise((resolve, reject) => {
+      var toReturn = {};
+      toReturn['matches'] = [];
+      toReturn['conflicts'] = [];
+      toReturn['new'] = [];
+      var numFormulasProcessed = 0;
+      formulas.forEach(formula => {
+        console.log(formula);
+        //do processing here
+        numFormulasProcessed = numFormulasProcessed + 1;
+        if (numFormulasProcessed == formulas.length) {
+          resolve(toReturn);
+        }
+      });
+    });
   }
 
   /**
@@ -52,16 +206,45 @@ export class ImportMatchConflictNewCheckerService {
    * Product Line
    * Formula
    */
-  private checkSKUReferences(skus, productLines, formulas): any {
-
+  private checkSKUReferences(skus, productLines, formulas): Promise<any> {
+    return new Promise((resolve, reject) => {
+      var toReturn = {};
+      toReturn['matches'] = [];
+      toReturn['conflicts'] = [];
+      toReturn['new'] = [];
+      var numFormulasProcessed = 0;
+      formulas.forEach(formula => {
+        console.log(formula);
+        //do processing here
+        numFormulasProcessed = numFormulasProcessed + 1;
+        if (numFormulasProcessed == formulas.length) {
+          resolve(toReturn);
+        }
+      });
+    });
   }
 
   /**
    * Objects Manufacturing Lines reference that could cause errors:
    * SKUs
    */
-  private checkManufacturingLineReferences(manufacturingLines, skus): any {
-
+  private checkManufacturingLineReferences(manufacturingLines, skus): Promise<any> {
+    return new Promise((resolve, reject) => {
+      var toReturn = {};
+      toReturn['matches'] = [];
+      toReturn['conflicts'] = [];
+      toReturn['new'] = [];
+      //do processing here
+      //can't do this yet because I'm blocked since SKU support is not ready in rest.service.ts yet
+      var numSKUsProcessed = 0;
+      skus.forEach(sku => {
+        console.log(sku);
+        numSKUsProcessed = numSKUsProcessed + 1;
+        if (numSKUsProcessed == skus.length) {
+          resolve(toReturn);
+        }
+      });
+    });
   }
 
 }
--- src/app/import/import.component.ts
+++ src/app/import/import.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatDialog } from '@angular/material';
 import { RestService } from '../rest.service';
 import { ParseCsvService } from '../parse-csv.service';
+import { ImportMatchConflictNewCheckerService } from '../import-match-conflict-new-checker.service';
 
 @Component({
   selector: 'app-upload',
@@ -11,7 +12,7 @@ import { ParseCsvService } from '../parse-csv.service';
 export class ImportComponent  implements OnInit {
   @ViewChild('fileSelector') fileSelector;
 
-  constructor(private parser: ParseCsvService, public dialog: MatDialog, public rest:RestService) {}
+  constructor(private parser: ParseCsvService, private importChecker: ImportMatchConflictNewCheckerService, public dialog: MatDialog, public rest:RestService) {}
 
   openFileSelector() {
     this.fileSelector.nativeElement.click();
@@ -21,8 +22,13 @@ export class ImportComponent  implements OnInit {
   }
 
   filesSelected() {
-    this.parser.parseCSVFiles(this.fileSelector.nativeElement.files).then(result => {
-      console.log(result);
+    this.parser.parseCSVFiles(this.fileSelector.nativeElement.files).then(csvResult => {
+      console.log(csvResult);
+      this.importChecker.checkAll(csvResult).then(checkResult => {
+        console.log(checkResult);
+      }).catch(err => {
+        console.log(err);
+      });
     }).catch(err => {
       console.log(err);
     });
