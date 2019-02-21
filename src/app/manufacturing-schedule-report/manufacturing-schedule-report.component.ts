33ac0f088b83c6399ea8fd5f3b7c49f2c45f7b45
Dylan Peters <dlp22@duke.edu>
2019-02-21 16:51:35 -0500

    Added manufacturing line dropdown.

--- src/app/manufacturing-schedule-report/manufacturing-schedule-report.component.ts
+++ src/app/manufacturing-schedule-report/manufacturing-schedule-report.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RestService } from '../rest.service';
 
 @Component({
   selector: 'app-manufacturing-schedule-report',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ManufacturingScheduleReportComponent implements OnInit {
 
-  constructor() { }
+  constructor(public rest:RestService) { }
+
+  lines: any[];
+  selectedLineName: string;
 
   ngOnInit() {
+    this.rest.getLine("",".*","",".*",100).subscribe(result => {
+      this.lines = result;
+    });
+  }
+
+  selectionChange(event) {
+    this.selectedLineName = event.value;
   }
 
 }
