2e185f434c44124118b7f895b36b96f7ff5df2b9
Dylan Peters <dlp22@duke.edu>
2019-02-21 18:13:30 -0500

    Passing ngModel to subcomponents.

--- src/app/manufacturing-schedule-report/manufacturing-schedule-report-line-table/manufacturing-schedule-report-line-table.component.ts
+++ src/app/manufacturing-schedule-report/manufacturing-schedule-report-line-table/manufacturing-schedule-report-line-table.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, forwardRef } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   selector: 'app-manufacturing-schedule-report-line-table',
   templateUrl: './manufacturing-schedule-report-line-table.component.html',
-  styleUrls: ['./manufacturing-schedule-report-line-table.component.css']
+  styleUrls: ['./manufacturing-schedule-report-line-table.component.css'],
+  providers: [{
+    provide: NG_VALUE_ACCESSOR,
+    useExisting: forwardRef(() => ManufacturingScheduleReportLineTableComponent),
+    multi: true
+  }]
 })
-export class ManufacturingScheduleReportLineTableComponent implements OnInit {
+export class ManufacturingScheduleReportLineTableComponent implements OnInit, ControlValueAccessor {
 
   constructor() { }
 
   ngOnInit() {
+  }
+  
+  _value = '';
+  stringified = '';
+
+  propagateChange: any = () => { };
+
+  writeValue(value: any) {
+    if (value) {
+      this._value = value;
+      this.stringified = JSON.stringify(value);
+    }
+  }
+
+  registerOnChange(fn) {
+    this.propagateChange = fn;
+  }
+  registerOnTouched(fn: () => void): void { }
+
+  onChange(event) {
+    this.stringified = JSON.stringify(event.target.value);
+    this.propagateChange(event.target.value);
   }
 
 }
--- src/app/manufacturing-schedule-report/manufacturing-schedule-report.component.ts
+++ src/app/manufacturing-schedule-report/manufacturing-schedule-report.component.ts
@@ -11,25 +11,27 @@ export class ManufacturingScheduleReportComponent implements OnInit {
   constructor(public rest:RestService) { }
 
   lines: any[];
-  selectedLine: string;
-  startDate: Date = new Date();
-  endDate: Date = new Date(new Date().setUTCFullYear(new Date().getUTCFullYear()+1));
+  options: any = {
+    selectedLine:null,
+    startDate: new Date(),
+    endDate: new Date(new Date().setUTCFullYear(new Date().getUTCFullYear()+1))
+  }
 
   ngOnInit() {
     this.rest.getLine("",".*","",".*",100).subscribe(result => {
       this.lines = result;
       if (result.length > 0) {
-        this.selectedLine = result[0]['linename'];
+        this.options.selectedLine = result[0]['linename'];
       }
       this.selectionChange();
     });
   }
 
   selectionChange() {
-    if (this.selectedLine && this.startDate && this.endDate) {
-      console.log(this.selectedLine);
-      console.log(this.startDate);
-      console.log(this.endDate);
+    if (this.options && this.options.selectedLine && this.options.startDate && this.options.endDate) {
+      console.log(this.options.selectedLine);
+      console.log(this.options.startDate);
+      console.log(this.options.endDate);
     }
   }
 
