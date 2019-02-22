bdae91c3cf374f1d9d13d5d57a48842ef1092b4f
Faith <faithrodriguez08@gmail.com>
2019-02-22 15:02:58 -0800

    product lines finished

--- src/app/product-line/delete-product-line-dialog.component.ts
+++ src/app/product-line/delete-product-line-dialog.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit, Inject} from '@angular/core';
 import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
+import { RestService } from '../rest.service';
 
 @Component({
     selector: 'app-delete-product-line-dialog',
@@ -7,17 +8,38 @@ import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
   })
   export class DeletePLDialogComponent implements OnInit {
 
-    productlines: [];
-    constructor(@Inject(MAT_DIALOG_DATA) public data: any, private dialogRef: MatDialogRef<DeletePLDialogComponent>) {}
+    productlines: String[];
+    constructor(@Inject(MAT_DIALOG_DATA) public data: any, public rest: RestService, private dialogRef: MatDialogRef<DeletePLDialogComponent>) {}
   
+    selectedOptions: String[] = [];
+
     ngOnInit() {
       this.productlines = this.data;
     }
 
     closeDialog() {
+      this.productlines = this.selectedOptions;
+      console.log("closing selected", this.selectedOptions)
+      this.data = this.selectedOptions;
+      console.log("closing data", this.data)
+      this.deleteProductLines();
       this.dialogRef.close();
-      console.log(this.data)
-      this.productlines = this.data;
+    }
+
+    onNgModelChange(event){
+      this.selectedOptions = event;
+      this.productlines = this.selectedOptions;
+      console.log(this.selectedOptions)
     }
   
+    deleteProductLines() {
+      let i;
+        for (i=0; i<this.selectedOptions.length; i++) {
+          this.rest.deleteProductLine(this.selectedOptions[i]).subscribe(results => {
+            if (results != null) {
+              console.log(results)
+            }
+          })
+        }
+    }
   }
