2d0d5cc7e2609c0d345344c71df8fe78f271bc89
ben linde <bblinde@yahoo.com>
2019-02-22 16:51:00 -0500

    Finished New Goal Dialog

--- src/app/rest.service.ts
+++ src/app/rest.service.ts
@@ -3,10 +3,10 @@ import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http
 import { auth } from './auth.service'
 import { Observable } from 'rxjs';
 
-// const endpoint = 'https://vcm-8238.vm.duke.edu:8443/'; // Ben
+const endpoint = 'https://vcm-8238.vm.duke.edu:8443/'; // Ben
 // const endpoint = 'https://vcm-8405.vm.duke.edu:8443/'; // Noah
 // const endpoint = 'https://vcm-8205.vm.duke.edu:8443/'; // Prod
-const endpoint = 'https://localhost:8443/'; // localhost
+// const endpoint = 'https://localhost:8443/'; // localhost
 
 @Injectable({
   providedIn: 'root'
@@ -378,8 +378,6 @@ deleteSku(skuName: String): Observable<any> {
 
   ///////////////////// Manufacturing Lines /////////////////////
   getLine(linename: String, linenameregex: String, shortname: String, shortnameregex: String, limit: number): Observable<any> {
-    console.log(linename);
-    console.log(shortname);
     return this.http.get(endpoint + 'manufacturing-lines', this.generateHeader({
       linename: linename,
       linenameregex: linenameregex,
