6be3c19b5f57aa9c7325c4f649e6ee22dbf8346c
ncp14 <noah.pritt@duke.edu>
2019-02-25 20:40:05 -0500

    improved junky formatting

--- src/app/sku/sku.component.ts
+++ src/app/sku/sku.component.ts
@@ -82,7 +82,6 @@ export class SkuComponent  implements OnInit {
   }
 
   refreshData(filterQueryData?) {
-    console.log("first things first: refresh da data");
     filterQueryData = filterQueryData ? ".*"+filterQueryData+".*" : ".*"+this.filterQuery+".*"; //this returns things that have the pattern anywhere in the string
     this.rest.getSkus("", filterQueryData, null, null, null, "", this.paginator.pageSize*10).subscribe(response => {
       this.data = response;
