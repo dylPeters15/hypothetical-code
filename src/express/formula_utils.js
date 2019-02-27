4fb5b12d047bfb71d8fe71ad42e529066b75a52f
Dylan Peters <dlp22@duke.edu>
2019-02-27 15:46:56 -0500

    Modified formulas.

--- src/express/formula_utils.js
+++ src/express/formula_utils.js
@@ -1,33 +1,14 @@
 const database = require('./database.js');
 const mongoose = require('mongoose');
 
-function getFormulas(formulaname, formulanameregex, formulanumber, ingredient, limit) {
+function getFormulas(filterSchema) {
     console.log("get formula.");
     return new Promise((resolve, reject) => {
-        console.log(ingredient);
-        var orClause = [];
-        if (formulaname) {
-            orClause.push({ formulaname: formulaname });
-        }
-        if (formulanumber) {
-            orClause.push({ formulanumber: formulanumber });
-        }
-        if (ingredient != "" && ingredient != undefined) {
-            orClause.push({ "ingredientsandquantities.ingredient" : ingredient});
-        }
-        if(formulanameregex) {
-            orClause.push({ formulaname: { $regex: formulanameregex }});
-        }
-        var filterSchema = {
-            $or: orClause
-        } 
-        console.log(filterSchema);
         database.formulaModel.find(filterSchema).limit(limit).populate('ingredientsandquantities.ingredient').exec(function(err, formulas) {
-            if(formulas != undefined && formulas != null){
+            if(err){
+                reject(Error(err));
+              } else {
                 resolve(formulas);
-              }
-              else {
-                  reject(Error(err));
               }  
         });
     });
@@ -35,9 +16,6 @@ function getFormulas(formulaname, formulanameregex, formulanumber, ingredient, l
 
 function createFormula(newFormulaObject) {
     return new Promise((resolve, reject) => {
-        // for (var i = i; i < newFormulaObject['ingredientsandquantities'].length; i++) {
-        //     newFormulaObject['ingredientsandquantities'][i]['ingredient'] = mongoose.Types.ObjectId(newFormulaObject['ingredientsandquantities'][i]['ingredient']);
-        // }
         let formula = new database.formulaModel(newFormulaObject);
         formula.save().then(response => {
             resolve(response);
@@ -47,45 +25,8 @@ function createFormula(newFormulaObject) {
     });
 }
 
-function createFormula(formulaname, formulanumber, 
-    ingredientsandquantities, comment) {
-    
-    return new Promise(function (resolve, reject) {
-        createUniqueFormulaNumber().then(response => {
-            var newformulanumber;  
-
-            if (formulanumber == null) {
-                newformulanumber = Number(response)
-            }
-            else {
-                newformulanumber = formulanumber
-            }
-            let formula = new database.formulaModel({
-                formulaname: formulaname,
-                formulanumber: newformulanumber,
-                ingredientsandquantities: ingredientsandquantities,
-                comment: comment
-            });
-
-            formula.save().then(result => {
-                console.log("result: " + result);
-                resolve(result);
-            }).catch(err => {
-                reject(Error(err));
-            });
-        }).catch(err => {
-            throw(Error(err));
-        });
-        
-    });
-}
-
-
-function modifyFormula(formulaname, newFormulaObject) {
+function modifyFormula(filterSchema, newFormulaObject) {
     return new Promise((resolve, reject) => {
-        var filterSchema = {
-            formulaname: formulaname
-        }
         database.formulaModel.updateOne(filterSchema, newFormulaObject, (err, response) => {
             if (err) {
                 reject(Error(err));
@@ -96,12 +37,9 @@ function modifyFormula(formulaname, newFormulaObject) {
     });
 }
 
-function deleteFormula(formulanumber) {
+function deleteFormula(filterSchema) {
     return new Promise(function (resolve, reject) {
-        const filterschema = {
-            formulanumber: formulanumber
-        };
-        database.formulaModel.deleteOne(filterschema, (err, response) => {
+        database.formulaModel.deleteOne(filterSchema, (err, response) => {
             if (err) {
                 reject(Error(err));
                 return
@@ -111,29 +49,29 @@ function deleteFormula(formulanumber) {
     });    
 }
 
-function createUniqueFormulaNumber() {
-    var newformulanumber;
-    var numFound = false;
-    while (!numFound) {
-        newformulanumber = Math.round(Math.random() * 100000000);
-        return new Promise(function (resolve, reject) {
-            database.formulaModel.find({ 'formulanumber':newformulanumber }, 'formulanumber').exec((err,result)=> {
-                if (result == null) {
-                    numFound = true;
-                }
-                if (err) {
-                    reject(Error(err));
-                }
-                if (result != null) {
-                    numFound = false;
-                }
+// function createUniqueFormulaNumber() {
+//     var newformulanumber;
+//     var numFound = false;
+//     while (!numFound) {
+//         newformulanumber = Math.round(Math.random() * 100000000);
+//         return new Promise(function (resolve, reject) {
+//             database.formulaModel.find({ 'formulanumber':newformulanumber }, 'formulanumber').exec((err,result)=> {
+//                 if (result == null) {
+//                     numFound = true;
+//                 }
+//                 if (err) {
+//                     reject(Error(err));
+//                 }
+//                 if (result != null) {
+//                     numFound = false;
+//                 }
              
                  
-            });
-            resolve(newformulanumber);  
-        });
-    }
-}
+//             });
+//             resolve(newformulanumber);  
+//         });
+//     }
+// }
 
 module.exports = {
     getFormulas: getFormulas,
