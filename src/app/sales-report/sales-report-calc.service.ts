011aff2c4878b33e14355783c11b1c58bb43d19a
Dylan Peters <dlp22@duke.edu>
2019-03-07 19:39:31 -0500

    Added salesByYear var.

--- src/app/sales-report/sales-report-calc.service.ts
+++ src/app/sales-report/sales-report-calc.service.ts
@@ -8,6 +8,23 @@ export class SalesReportCalcService {
   constructor() { }
 
   async summarizeSales(allSales: any[]): Promise<any> {
-    return allSales;
+    var currentDate: Date = new Date();
+    var currentYear: Number = currentDate.getFullYear();
+    console.log(currentYear);
+
+    var salesByYear = {};
+    for (let sale of allSales) {
+      var dateOfSale: Date = new Date(sale['date']);
+      var yearOfSale = dateOfSale.getFullYear();
+      console.log("Date of Sale: ", dateOfSale);
+      if (!salesByYear[yearOfSale]){
+        salesByYear[yearOfSale] = [];
+      }
+      salesByYear[yearOfSale].push(sale);
+    }
+    console.log("Sales by year: ", salesByYear);
+    var summarized = [];
+
+    return summarized;
   }
 }
