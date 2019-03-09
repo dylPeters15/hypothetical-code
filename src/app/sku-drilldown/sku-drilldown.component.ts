bff64ca7f885e568141590db312363a646bcbca1
Dylan Peters <dlp22@duke.edu>
2019-03-09 00:07:28 -0500

    SKU Drilldown displays customer options.

--- src/app/sku-drilldown/sku-drilldown.component.ts
+++ src/app/sku-drilldown/sku-drilldown.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from "@angular/material";
+import { RestServiceV2, AndVsOr } from '../restv2.service';
 
 @Component({
   selector: 'app-sku-drilldown',
@@ -9,18 +10,58 @@ import { MatDialogRef, MAT_DIALOG_DATA } from "@angular/material";
 export class SkuDrilldownComponent implements OnInit {
 
   sku: any = {};
+
+  customers: any[] = [];
+  allCustomersSelected: boolean = true;
   selectedCustomers: any[] = [];
 
-  constructor(private dialogRef: MatDialogRef<SkuDrilldownComponent>, @Inject(MAT_DIALOG_DATA) public initData: any) { }
+  constructor(public restv2: RestServiceV2, private dialogRef: MatDialogRef<SkuDrilldownComponent>, @Inject(MAT_DIALOG_DATA) public initData: any) { }
 
   ngOnInit() {
-    console.log(this.initData);
     if (this.initData['sku']) {
       this.sku = this.initData['sku'];
     }
-    if (this.initData['selectedCustomers']) {
-      this.selectedCustomers = this.initData['selectedCustomers'];
+    this.onInit().then(() => {
+      console.log(this.initData);
+      for(var i = 0; i < this.customers.length && this.initData && this.initData['selectedCustomers']; i++) {
+        this.customers[i]['checked'] = this.initData['selectedCustomers'].filter((value,index,array) => {
+          return value['customername'] == this.customers[i]['customername'];
+        }).length == 1;
+      }
+      this.refreshSelected();
+    });
+  }
+
+  async onInit(): Promise<void> {
+    this.customers = await this.restv2.getCustomers(AndVsOr.OR, null, null, null, 10000);
+    for (let customer of this.customers) {
+      customer['checked'] = true;
     }
+    this.refreshSelected();
+  }
+
+  refreshSelected(): void {
+    var areAllSelected = true;
+    for (let customer of this.customers) {
+      if (!customer['checked']) {
+        areAllSelected = false;
+      }
+    }
+    this.allCustomersSelected = areAllSelected;
+    this.selectedCustomers = this.customers.filter((value, index, array) => {
+      return value['checked'];
+    });
+  }
+
+  customerSelectionsChanged(): void {
+    this.refreshSelected();
+  }
+
+  selectDeselectAllCustomers(): void {
+    for (let customer of this.customers) {
+      customer['checked'] = this.allCustomersSelected;
+    }
+    this.refreshSelected();
   }
 
 }
