ec6c9bd81b9b4653d7084f1daf989c1f20e6fb18
ben linde <bblinde@yahoo.com>
2019-03-18 21:17:09 -0400

    Scrapes website for sales data and stores in database, asynchronously

--- src/express/v2/sales_scraper.js
+++ src/express/v2/sales_scraper.js
@@ -1,5 +1,7 @@
 const rp = require('request-promise');
+const sales_utils = require('./sales_utils.js')
 const database = require('../database.js');
+const $ = require('cheerio');
 const url = 'http://hypomeals-sales.colab.duke.edu:8080'; // /?sku=1&year=2010
 
 
@@ -11,8 +13,7 @@ async function scrapeAll(){
   });
     for(sku of result) {
       var year = 0;
-      for(year = 1999; year <= 2019; year++){
-        
+      for(year = 1999; year <= new Date().getFullYear(); year++){
           await scrape(sku.skunumber, year);
       }
 
@@ -23,14 +24,50 @@ async function scrapeAll(){
 
 async function scrape(skunumber, year){
   let urlToParse = url + '/?sku=' + skunumber + '&year=' + year;
-  console.log("URL: " + urlToParse)
+  var sku = await database.skuModel.findOne({
+    skunumber: skunumber
+  }).exec().catch(err => {
+    if(err){
+      throw err;
+    }
+  });
     rp(urlToParse)
-    .then(function(html){
+    .then(function(html){  
       //success!
-      console.log(html);
+      const sales = [];
+      var count = 0;
+      $('tr', html).each(function(i, tr){
+        if(count > 0){
+          var children = $(this).children();
+          var currentCustomerTuple = {
+            customernumber: children.eq(3).text(),
+            customername: children.eq(4).text()
+          };
+          var day = (1 + (Number(children.eq(2).text()) - 1) * 7)
+          let date = new Date(year, 0, day);
+          var customer = database.customerModel.findOneAndUpdate(currentCustomerTuple, currentCustomerTuple, function(err, result){
+            if(err){
+              throw err;
+            }
+            
+            var sale = {
+              "date": date,
+              "sku": sku['_id'],
+              "customer":  result['id'],
+              "numcases": children.eq(5).text(),
+              "pricepercase": children.eq(6).text().trim()
+          };
+          sales_utils.createSale(sale);
+          });
+
+        }
+        count++;
+      });
     })
     .catch(function(err){
-      //handle error
+      if(err){
+        throw err;
+      }
     });
 }
 
