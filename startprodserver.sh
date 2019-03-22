1ca8e8b73f86833d5c007b323b92e12eeb2e49f5
Dylan Peters <dlp22@duke.edu>
2019-03-22 12:46:55 -0400

    State of prod before deploying ev 3 for the first time.

--- startprodserver.sh
+++ startprodserver.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+cd /hypothetical-code/hypothetical-code
 sudo service mongod start
 npm install
 nohup npm run startapi & disown
 nohup npm run startangularprod & disown
 nohup npm run starthttpredirectprod & disown
+cd -
