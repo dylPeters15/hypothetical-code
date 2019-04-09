1bcd03267267b61f3e7aa33dd35273c1c54f2b51
Faith <faithrodriguez08@gmail.com>
2019-04-08 22:46:17 -0700

    Correctly finds valid lines

--- src/app/manufacturing-schedule/auto-schedule.component.ts
+++ src/app/manufacturing-schedule/auto-schedule.component.ts
@@ -6,17 +6,19 @@ import { Router } from '@angular/router';
 import { MatSnackBar } from '@angular/material';
 import {FormControl} from '@angular/forms';
 import {MatDatepickerInputEvent} from '@angular/material/datepicker';
+import { auth } from '../auth.service';
+import { LineToLineMappedSource } from 'webpack-sources';
 
 export class ActivityData {
     selected: Boolean;
-    name: String;
-    id: Object;
-    goalName: String
+    name: string;
+    activity: Object;
+    goalName: string
 
-    constructor(name, id, goalName) {
+    constructor(name, activity, goalName) {
         this.selected = false;
         this.name = name;
-        this.id = id;
+        this.activity = activity;
         this.goalName = goalName
     }
 }
@@ -42,12 +44,16 @@ export class AutoScheduleComponent implements OnInit {
     endDate = new FormControl(new Date());
     startSet: Boolean = false;
     endSet: Boolean = false;
+    unscheduledActivityList: any[] = [];
+    scheduledActivityList: any[] = [];
+    manufacturingLinesToManage: any[] = [];
 
     ngOnInit() {
         // this.paginator.pageSize = 20;
         // this.paginator.page.subscribe(event => {
         //   // this.deselectAll();
         // });
+        this.refreshMLs();
         this.refreshData();
     }
 
@@ -61,13 +67,18 @@ export class AutoScheduleComponent implements OnInit {
                     console.log('activity', activity)
                     if (activity['activity']['line'] == null || activity['activity']['line'] == undefined) {
                         console.log('add to table')
-                        let activityTable = new ActivityData(activity['activity']['sku']['skuname'], ['activity']['_id'], goal['goalname'])
+                        this.unscheduledActivityList.push(activity['activity']);
+                        let activityTable = new ActivityData(activity['activity']['sku']['skuname'], activity['activity'], goal['goalname'])
                         this.data.push(activityTable)
+                    }
+                    else {
+                        this.scheduledActivityList.push(activity['activity']);
                     }
                 });
                 
             }
         })
+        console.log('activity data', this.data)
         this.dataSource =  new MatTableDataSource<ActivityData>(this.data);
     }
 
@@ -81,10 +92,72 @@ export class AutoScheduleComponent implements OnInit {
         this.endSet = true;
     }
 
-    createSchedule() {
+    async createSchedule(): Promise<void> {
+        var selectedActivities: any[] = [];
+        var thisObject = this;
+        var wait = new Promise((resolve, reject) => {
+            this.data.forEach(async (activity, index, array) => {
+                var goal = await this.restv2.getGoals(AndVsOr.OR, null, 
+                    activity['goalName'], null, true, 1);
+                if (goal.length == 1) {
+                    console.log('goal and activity', goal[0], activity)
+                    if (activity.selected == true) {
+                        console.log('is selected', activity.selected)
+                        var deadline = new Date(goal[0]['date']);
+                        console.log('deadline', deadline)
+                        selectedActivities.push([deadline, activity['activity']['calculatedhours'], activity['activity']]);    
+                    }
+                    if (index === array.length -1) resolve();
+                }  
+            })
+        });
+        wait.then(async () => {
+            selectedActivities.sort(thisObject.sortFunction)
+            console.log('selectedActivities sorted', selectedActivities)
+            selectedActivities.forEach(async (activity) => {
+                this.getValidLines(activity).then(potentialLines => {
+                    console.log(potentialLines);
+                    
+                })
+            })
+        })
+        
 
 
         this.dialogRef.close();
+    }
+
+    sortFunction(a, b) {
+        if (a[0].valueOf() === b[0].valueOf()) {
+            if (a[1] === b[1]) {
+                return -1;
+            }
+            else {
+                return (a[1] < b[1]) ? -1 : 1;
+            }
+        }
+        else {
+            return (a[0].valueOf() < b[0].valueOf()) ? -1 : 1;
+        }
+    }
+
+    async getValidLines(activity): Promise<any> {
+        var potentialLines = []
+        this.manufacturingLinesToManage.forEach(line => {    
+            console.log('line with access', line)
+            var sku = activity[2]['sku']; 
+            console.log('sku', sku);
+            var lineSkus = []
+            line['manufacturingline']['skus'].forEach(lineSku => {    
+                lineSkus.push(lineSku['sku']);
+            })
+            if (lineSkus.includes(sku['_id'])) {
+                potentialLines.push(line);
+            }
+            console.log('potentialLines', potentialLines)
+        })  
+        console.log('final line')
+        return potentialLines
     }
  
     deselectAll() {
@@ -104,11 +177,30 @@ export class AutoScheduleComponent implements OnInit {
     }
 
     checkValid(): boolean {
-        if (this.startSet && this.endSet) {
+        var isSelected = false;
+        this.data.forEach(activity => {
+            if(activity.selected) {
+                isSelected = true;
+            }
+        })
+        if (this.startSet && this.endSet && isSelected) {
             return false;
         }
         else {
             return true;
+        }
+    }
+
+    async refreshMLs(): Promise<void> {
+        var thisobject = this;
+        var users = await thisobject.restv2.getUsers(AndVsOr.AND, auth.getUsername(), null, null, null, null, null, null, auth.getLocal(), 1);
+        if (users.length == 1) {
+        //   this.manufacturingLinesToManage = [];
+        //   users[0].manufacturinglinestomanage.forEach(line => {
+        //       this.manufacturingLinesToManage.push(line['manufacturingschedule']['_id'])
+        //   });
+        this.manufacturingLinesToManage = users[0].manufacturinglinestomanage;
+        console.log(this.manufacturingLinesToManage)
         }
     }
 }
