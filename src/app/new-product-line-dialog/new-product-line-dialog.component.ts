21cefab7672f876876245b7fe4eb6389b58e378c
Dylan Peters <dlp22@duke.edu>
2019-04-14 22:57:05 -0400

    Product line name is now unique.

--- src/app/new-product-line-dialog/new-product-line-dialog.component.ts
+++ src/app/new-product-line-dialog/new-product-line-dialog.component.ts
@@ -1,33 +1,52 @@
-import {Component, OnInit, Inject} from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { RestServiceV2, AndVsOr } from '../restv2.service';
-import {MatDialog,MatSnackBar, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
+import { MatDialog, MatSnackBar, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { FormControl, FormGroupDirective } from '@angular/forms';
 
 @Component({
-    selector: 'app-new-product-line-dialog',
-    templateUrl: 'new-product-line-dialog.component.html',
-  })
-  export class NewProductLineDialogComponent implements OnInit {
-    nameExists: boolean = false;
-    productlinename: String;
-    constructor(public restv2: RestServiceV2, @Inject(MAT_DIALOG_DATA) public data: any, private dialogRef: MatDialogRef<NewProductLineDialogComponent>) {}
-  
-    async ngOnInit() {
-      this.productlinename = this.data.productlinename;
-    }
+  selector: 'app-new-product-line-dialog',
+  templateUrl: 'new-product-line-dialog.component.html',
+})
+export class NewProductLineDialogComponent implements OnInit {
+  productlinename;
+  constructor(public restv2: RestServiceV2, @Inject(MAT_DIALOG_DATA) public data: any, private dialogRef: MatDialogRef<NewProductLineDialogComponent>) { }
 
-    closeDialog() {
-      this.dialogRef.close();
-      this.productlinename = this.data.productlinename;
-    }
+  async ngOnInit() {
+    console.log(this.data);
+    this.productlinename = this.data.productlinename;
+  }
 
-    onNoClick() {
-      this.dialogRef.close();
-    }
+  onNoClick() {
+    this.dialogRef.close();
+  }
 
-    async createProductLine() {
-      if (this.data.productlinename) {
-        this.dialogRef.close(this.data.productlinename);
+  createProductLine() {
+    if (this.productlinename && !this.productlinenameError) {
+      this.dialogRef.close(this.productlinename);
+    }
+  }
+
+  productlinenameError = false;
+  productlinenameErrorMatcher = {
+    isErrorState: (control: FormControl, form: FormGroupDirective): boolean => {
+      return this.productlinenameError;
+    }
+  }
+  async productlinenameChanged() {
+    if (this.data) {
+      if (this.productlinename == this.data.productlinename) {
+        this.productlinenameError = false;
+        return;
       }
     }
-  
+    console.log(this.productlinename);
+    var result = await this.restv2.getProductLines(AndVsOr.OR, this.productlinename, null, 1);
+    console.log(result);
+    if (result.length == 1) {
+      this.productlinenameError = true;
+      return;
+    }
+    this.productlinenameError = false;
   }
+
+}
